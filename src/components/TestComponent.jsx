cfe5870a4f2fabd878baa5f7aeb5a497b86433c7
Zefanja Timorason <z.timorason@gmail.com>
2023-10-31 22:34:26 +0100

    played around some more with the test component

--- src/components/TestComponent.jsx
+++ src/components/TestComponent.jsx
@@ -1,27 +1,35 @@
-import React, { useState, useEffect } from "react";
+import { useState, useCallback, useEffect } from "react";
 import axios from "axios";
 
 const TestComponent = () => {
   const [data, setData] = useState([]);
 
-  const getData = () => {
+  const getData = useCallback(() => {
     axios
       .get("http://localhost:5005/api/recipes")
       .then((res) => {
         setData(res.data);
       })
       .catch((err) => console.log(err));
-  };
+  }, []);
 
   useEffect(() => {
     getData();
+    console.log("data", data);
   }, []);
-  console.log(data);
+
   return (
     <div>
       <h1>Test Component</h1>
       {data.map((data) => (
-        <p key={data._id}>{data.Title}</p>
+        <div key={data._id}>
+          <p className="text-2xl" key={data._id}>
+            {data.Title}
+          </p>
+          {data.Image.map((img, index) => (
+            <img className="w-40 h-40" key={index} src={img} alt="recipe" />
+          ))}
+        </div>
       ))}
     </div>
   );
