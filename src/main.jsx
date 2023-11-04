af848cc7e9bcf0f79621c829c617d91df3b7b902
Zefanja Timorason <z.timorason@gmail.com>
2023-11-04 17:58:38 +0100

    added bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";
+// import "./index.css";
+import "./scss/styles.scss";
+// import * as bootstrap from "bootstrap";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,22 @@
-import { defineConfig } from "vite";
+// import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+
+// export default {
+//   plugins: [react()],
+//   rollupOptions: {
+//     external: ["react", "react-router", "react-router-dom"],
+//     output: {
+//       globals: {
+//         react: "React",
+//       },
+//     },
+//   },
+// };
+
+export default {
   plugins: [react()],
   rollupOptions: {
     external: ["react", "react-router", "react-router-dom"],
@@ -10,6 +24,10 @@ export default defineConfig({
       globals: {
         react: "React",
       },
+      root: path.resolve(__dirname, "frontend"),
+      build: {
+        outDir: "../dist",
+      },
     },
   },
-});
+};
